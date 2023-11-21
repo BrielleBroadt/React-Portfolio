38a8ca3e94a1503653c5df42d737cc68e353fcfa
BrielleBroadt <brielleb.broadt@gmail.com>
2023-11-21 16:07:38 -0500

    working on app.jsx and routes for pages

--- React Portfolio/src/App.jsx
+++ React Portfolio/src/App.jsx
@@ -1,9 +1,27 @@
 import Footer from "./components/Footer"
+import {createBrowserRouter, createRoutesFromElements, Route, Link, Outlet} from 'react-router-dom'
+import {Home} from './main.jsx'
+import {Contact} from './contact.jsx'
+import {Resume} from './resume.jsx'
+import {Projects} from './projects.jsx'
+import {About} from './about.jsx'
 
 
 
 function App() {
-
+  const router = createBrowserRouter (
+    createRoutesFromElements(
+      <Route path="/" element={<Root />} >
+        <Route index element={<Home />} />
+        <Route element={<Contact /> } />
+        <Route element={<Resume /> } />
+        <Route element={<Projects /> } />
+        <Route element={<About /> } />
+        
+        </Route>
+    )
+  )
+  
 
   return (
     <>
@@ -11,6 +29,22 @@ function App() {
      <Footer/>
     </>
   )
+}
+
+const Root = () => {
+  return
+   <>
+     <div>
+       <Link to="./components/About"> About </Link>
+       <Link to="./components/Contact"> Contact </Link>
+       <Link to="./components/Projects"> Projects </Link>
+       <Link to="./components/Resume"> Resume </Link>
+     </div>
+
+     <div>
+      <Outlet />
+     </div>
+   </>
 }
 
 export default App

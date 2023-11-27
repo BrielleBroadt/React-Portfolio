5a684248b3296193306ddfb3f94fb9070524691d
BrielleBroadt <brielleb.broadt@gmail.com>
2023-11-27 12:40:12 -0500

    edited footer and delpoyed to netlify

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faInstagram, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import './footer.css'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+    <div className="social-icons">
+      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+      <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </a>
+      <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+    </div>
+    <p>&copy; 2023 Your Portfolio</p>
+  </footer>
   )
 }
 

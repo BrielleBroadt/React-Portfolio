5edeb96fd38662f20a73712bf0e89acb91cf0ae9
BrielleBroadt <brielleb.broadt@gmail.com>
2023-11-28 17:04:12 -0500

    adding repos, links, images

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,9 +10,9 @@ const About = () => {
       <div className="about-me-content">
         <p>Welcome to my corner of the digital world! I'm Brielle Broadt, a recent graduate of the Rutgers University Coding Bootcamp, where I successfully earned my certificate as a Full stack Web Developer. Stepping into the world of web development has been an exciting journey. 
           Throughout the bootcamp I had the opportunity to work on diverse challanges and projects that you can find once you click projects!
-          My educational background has a wide vasriety with Full Stack Web Developer certification, 
+          My educational background has a wide variety with Full Stack Web Developer certification, 
           200 hour Registered Yoga Teacher, Associates of Science, and finishing up a Bachelors in Communications
-          at Rowan University. I am looking to gtaduate in 2025 and attend to Penn State for a Masters in Artificial Intelligence
+          at Rowan University. I am looking to graduate in 2025 and attend to Penn State for a Masters in Artificial Intelligence
           and one day have a PHD in AI!
           
           </p>
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import './projects.css'
+
 const projects = [
   {
     id: 1,
-    title: 'Project 1',
-    description: 'Description for Project 1',
-    imageUrl: 'URL_TO_IMAGE_1',
-    repoLink: 'URL_TO_REPO_1',
+    title: 'Witty-Web-Wizard',
+    description: 'Group project for Rutgers, API integration, HTML, javaScript, Tailwind',
+    imageUrl: 'https://github.com/BrielleBroadt/Witty-Web-Wizard/assets/135186013/c00d67a6-56c6-4a3c-a7dc-e3fd170f98a5',
+    repoLink: 'https://github.com/BrielleBroadt/Witty-Web-Wizard',
   },
   {
     id: 1,
-    title: 'Project 1',
-    description: 'Description for Project 1',
-    imageUrl: 'URL_TO_IMAGE_1',
-    repoLink: 'URL_TO_REPO_1',
+    title: 'SVG-Logo-Maker',
+    description: 'Inquierer, jest, javascript',
+    imageUrl: 'https://github.com/BrielleBroadt/Witty-Web-Wizard/assets/135186013/f747d4c5-27fd-43d6-80a7-9a5564d41e44',
+    repoLink: 'https://github.com/BrielleBroadt/SVG-Logo-Maker',
   },
   {
     id: 1,
-    title: 'Project 1',
-    description: 'Description for Project 1',
-    imageUrl: 'URL_TO_IMAGE_1',
-    repoLink: 'URL_TO_REPO_1',
+    title: 'Employee-Tracker',
+    description: 'sql, seeds, schema, server.js, database queries',
+    imageUrl: 'https://github.com/BrielleBroadt/Witty-Web-Wizard/assets/135186013/7f0a196f-a839-4902-b7d7-8c728be2d061',
+    repoLink: 'https://github.com/BrielleBroadt/Employee-Tracker',
   },
   {
     id: 1,
